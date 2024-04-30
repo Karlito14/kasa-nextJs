289af6caca78e10f820be62e88c620d760bd74e0
Karlito14 <leiroz26@hotmail.com>
2024-04-30 15:46:40 +0200

    correction affichage img dans la bannière

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -1,13 +1,13 @@
 import { StaticImageData } from 'next/image';
 
-interface Banner {
+interface BannerImg {
     img: StaticImageData;
     text?: string;
 }
 
-export const Banner = (props : Banner) => {
+export const Banner = (props : BannerImg) => {
     return (
-        <div className='container-banner md:h-56 md:justify-center md:mx-8' style={{backgroundImage: `url(${props.img})`}}>
+        <div className='container-banner md:h-56 md:justify-center md:mx-8' style={{backgroundImage: `url(${props.img.src})`}}>
             {props.text && <h1 className='text-white text-xl z-10 md:text-6xl'>{props.text}</h1>}
         </div>
     );
