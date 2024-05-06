e3cd80c9933f76a9c5bce0b92055feda962ee00c
Karlito14 <leiroz26@hotmail.com>
2024-05-06 14:13:58 +0200

    génération des pages statiques

--- app/(pages)/house/[id]/page.tsx
+++ app/(pages)/house/[id]/page.tsx
@@ -2,6 +2,12 @@ import { Error } from '@/app/components/Error/Error';
 import { HouseSheet } from '@/app/components/HouseSheet/HouseSheet';
 import { dataHouses } from '@/app/data/data';
 
+export function generateStaticParams() {
+    return dataHouses.map(house => {
+        return {house: house.id};
+    });
+}
+
 export default function housePage (props: { params: { id: string }}) {
     const [data] = dataHouses.filter(house => house.id === props.params.id);
 
