fa0de3636bcabad9694d47e727d665e133fb6d03
Karlito14 <leiroz26@hotmail.com>
2024-05-06 11:08:46 +0200

    ajout metadata et favicon

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,6 +2,16 @@ import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/header';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+ 
+export const metadata: Metadata = {
+    title: 'Kasa',
+    description: 'Kasa est dans le métier de la location d’appartements entre particuliers depuis près de 10 ans maintenant, Il fait partie des leaders sur le marché avec ses plus de 500 annonces quotidiennes',
+    icons: {
+        icon: ['/assets/favicon.png']
+    }
+};
 
 const montserrat = Montserrat({ subsets: ['latin']});
 
@@ -12,6 +22,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="fr">
+            
             <body className={montserrat.className}>
                 <Header />
                 {children}
